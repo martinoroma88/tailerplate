febc616df9c85e2dd83f1a8777eac16021edef69
Martino Roma <martinoroma88@gmail.com>
2021-04-18 15:54:59 +0200

    added JIT support and chaned npm scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-  purge: ["**/*.html"],
-  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  purge: ['public/**/*.html', 'public/**/*.js', 'tailwind.css'],
   theme: {
     extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  }
 }
